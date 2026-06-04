1ea3e32b58a9c52577271ddbe7fd19d43374ec83
Vitaly Nespirny <nespirny.v.n@econri.org>
2026-06-04 16:28:18 +0300

    chapter 1.2.4 solved

---
+++ 1.2.4.3.task/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const BIRTHDAY = '18.04.1982'; // константа, известная в момент написания скрипта -> UPPERCASE
+
+const age = someCode(BIRTHDAY); // константа, получающая значение во время выполнения скрипта -> camelCase
